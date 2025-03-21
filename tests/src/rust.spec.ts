44273b2ba5c5035e8f0fbd6dad622dfa107ee888
Felix Ableitner <me@nutomic.com>
2025-03-21 12:34:42 +0100

    fmt

--- tests/src/rust.spec.ts
+++ tests/src/rust.spec.ts
@@ -1,7 +1,14 @@
 jest.setTimeout(120000);
 
 import { CreatePostLike } from "lemmy-js-client";
-import { alpha, setupLogins, createPost, createCommunity, registerUser, alphaUrl } from "./shared";
+import {
+  alpha,
+  setupLogins,
+  createPost,
+  createCommunity,
+  registerUser,
+  alphaUrl,
+} from "./shared";
 
 beforeAll(async () => {
   await setupLogins();
@@ -18,15 +25,18 @@ test("Rust allowed voters", async () => {
 
   // user has no posts and is not allowed to vote
   let new_user = await registerUser(alpha, alphaUrl, "new_user");
-  let form: CreatePostLike = {post_id: postRes.post_view.post.id, score: -1};
-  await expect(new_user.likePost(form)).rejects.toStrictEqual(Error("plugin_error"));
+  let form: CreatePostLike = { post_id: postRes.post_view.post.id, score: -1 };
+  await expect(new_user.likePost(form)).rejects.toStrictEqual(
+    Error("plugin_error"),
+  );
 
   // create a few posts
-  for (var i= 0; i< 5; i++) { await createPost(
-    new_user,
-    community.community_view.community.id,
-    "test post number " + i,
-  );
+  for (var i = 0; i < 5; i++) {
+    await createPost(
+      new_user,
+      community.community_view.community.id,
+      "test post number " + i,
+    );
   }
 
   // now user is allowed to vote
