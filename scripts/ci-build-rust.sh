0551a50245ef14ecffb74e443d8a73e21bc67ff2
Felix Ableitner <me@nutomic.com>
2026-02-02 15:09:43 +0100

    fix target

--- scripts/ci-build-rust.sh
+++ scripts/ci-build-rust.sh
@@ -3,18 +3,16 @@ set -e
 
 NAME="$1"
 RELEASE="$2"
-TARGET="$3:=wasm32-unknown-unknown"
+TARGET="${3:-wasm32-unknown-unknown}"
 
 rustup target add $TARGET
 pushd "plugins/$NAME"
 
 
 if [ "$RELEASE" == "release" ]; then
-    echo "release build"
     cargo build --release
     cp "target/$TARGET/release/$NAME.wasm" ..
 else
-    echo debug
     cargo build
     cp "target/$TARGET/debug/$NAME.wasm" ..
 fi
